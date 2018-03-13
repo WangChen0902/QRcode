cdd7cfc08cefc4bc81425188ba106c133017d65a
WangChen0902 <827913668@qq.com>
2018-03-13 20:08:43 +0800

    测试1.0

--- QRcode/Program.cs
+++ QRcode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gma.QrCodeNet.Encoding;
 
 namespace QRcode
 {
@@ -10,6 +11,24 @@ namespace QRcode
     {
         static void Main(string[] args)
         {
+           
+            Console.Write(@"Type some text to QR code: ");
+            string sampleText = Console.ReadLine();
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.M);
+            QrCode qrCode = qrEncoder.Encode(sampleText);
+            for (int j = 0; j < qrCode.Matrix.Width; j++)
+            {
+                for (int i = 0; i < qrCode.Matrix.Width; i++)
+                {
+
+                    char charToPrint = qrCode.Matrix[i, j] ? '　' : '■';
+                    Console.Write(charToPrint);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(@"Press any key to quit.");
+            Console.ReadKey();
+            
         }
     }
 }
